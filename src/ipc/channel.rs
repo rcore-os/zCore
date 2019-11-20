e95540c0f964071b2f3eb3bb44812333e38632f1
PanQL <panqinglin00@163.com>
2019-11-20 23:12:46 +0800

    remove duplicate id() and move test for channel

--- src/ipc/channel.rs
+++ src/ipc/channel.rs
@@ -11,10 +11,6 @@ impl Channel {
     fn new(koid: KoID) -> Self {
         Channel { koid }
     }
-
-    pub fn id(&self) -> KoID {
-        self.koid
-    }
 }
 
 impl KernelObject for Channel {
@@ -33,4 +29,26 @@ pub fn create() -> (Handle, Handle) {
     let handle0 = Handle::new(Arc::new(Mutex::new(end0)), Rights::DUPLICATE);
     let handle1 = Handle::new(Arc::new(Mutex::new(end1)), Rights::DUPLICATE);
     (handle0, handle1)
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn is_work() {
+        use crate::ipc::channel::create;
+        use crate::object::handle::Handle;
+        use crate::ipc::channel::Channel;
+        use crate::error::*;
+        use crate::object::KernelObject;
+        let (handle0, handle1) = create();
+        handle0.do_mut(|ch: &mut Channel|{
+            assert_eq!(0u64, ch.id());
+            ZxError::OK
+        });
+        handle1.do_mut(|ch: &mut Channel|{
+            assert_eq!(1u64, ch.id());
+            ZxError::OK
+        });
+    }
 }
