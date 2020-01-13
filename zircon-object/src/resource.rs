83ba2b5cba06e2e50723a264937132ed7290ed1a
PanQL <panqinglin00@163.com>
2020-01-13 16:43:03 +0800

    add definition for ResourceKind

--- zircon-object/src/resource.rs
+++ zircon-object/src/resource.rs
@@ -1,17 +1,29 @@
-use crate::object::*;
-use alloc::string::String;
-use alloc::sync::Arc;
+use {crate::object::*, alloc::string::String, alloc::sync::Arc};
+
+/// ResourceKind definition from fuchsia/zircon/system/public/zircon/syscalls/resource.h
+#[repr(u32)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum ResourceKind {
+    MMIO = 0,
+    IRQ = 1,
+    IOPORT = 2,
+    HYPERVISOR = 3,
+    ROOT = 4,
+    VMEX = 5,
+    SMC = 6,
+    COUNT = 7,
+}
 
 pub struct Resource {
     base: KObjectBase,
     name: String,
-    kind: u32,
+    kind: ResourceKind,
 }
 
 impl_kobject!(Resource);
 
 impl Resource {
-    pub fn create(name: &str, kind: u32) -> ZxResult<Arc<Self>> {
+    pub fn create(name: &str, kind: ResourceKind) -> ZxResult<Arc<Self>> {
         Ok(Arc::new(Resource {
             base: KObjectBase::new(),
             name: String::from(name),
@@ -19,7 +31,7 @@ impl Resource {
         }))
     }
 
-    pub fn validate(&self, kind: u32) -> ZxResult<()> {
+    pub fn validate(&self, kind: ResourceKind) -> ZxResult<()> {
         return if self.kind == kind {
             Ok(())
         } else {
