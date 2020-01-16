6063c681fd82500a7fc29d84509d0544dc8b049d
Runji Wang <wangrunji0408@163.com>
2020-01-16 17:07:43 +0800

    use structopt and move code to lib in zircon_userboot

--- zircon-userboot/src/lib.rs
+++ zircon-userboot/src/lib.rs
@@ -1,22 +1,127 @@
 #![no_std]
+#![feature(asm)]
+#![feature(naked_functions)]
 #![deny(warnings, unused_must_use)]
 
+#[macro_use]
 extern crate alloc;
 
 extern crate log;
 
 use {
-    alloc::sync::Arc,
+    alloc::{sync::Arc, vec::Vec},
     xmas_elf::{
         program::{ProgramHeader, SegmentData, Type},
         sections::SectionData,
         symbol_table::Entry,
         ElfFile,
     },
-    zircon_object::{vm::*, ZxError, ZxResult},
+    zircon_object::{
+        ipc::channel::*,
+        object::*,
+        resource::{Resource, ResourceKind},
+        task::*,
+        vm::*,
+        ZxError, ZxResult,
+    },
+    zircon_syscall::Syscall,
 };
 
 mod vdso;
+
+pub fn run_userboot(userboot_data: &[u8], vdso_data: &[u8], zbi_data: &[u8], cmdline: &str) {
+    let job = Job::root();
+    let proc = Process::create(&job, "proc", 0).unwrap();
+    let thread = Thread::create(&proc, "thread", 0).unwrap();
+    let resource = Resource::create("root", ResourceKind::ROOT).unwrap();
+    let vmar = proc.vmar();
+
+    const VBASE: usize = 0x400000000;
+
+    // userboot
+    let (entry, userboot_size) = {
+        let elf = ElfFile::new(userboot_data).unwrap();
+        let size = elf.load_segment_size();
+        let vmar = vmar.create_child(VBASE, size).unwrap();
+        vmar.load_from_elf(&elf).unwrap();
+        (VBASE + elf.header.pt2.entry_point() as usize, size)
+    };
+
+    // vdso
+    let vdso_vmo = {
+        let elf = ElfFile::new(vdso_data).unwrap();
+        let size = elf.load_segment_size();
+        let syscall_entry_offset = elf
+            .get_symbol_address("zcore_syscall_entry")
+            .expect("failed to locate syscall entry") as usize;
+        let vmar = vmar.create_child(VBASE + userboot_size, size).unwrap();
+        let first_vmo = vmar.load_from_elf(&elf).unwrap();
+
+        unsafe {
+            // fill syscall entry
+            ((VBASE + userboot_size + syscall_entry_offset) as *mut usize)
+                .write(syscall_entry as usize);
+        }
+        first_vmo
+    };
+
+    // zbi
+    let zbi_vmo = {
+        let vmo = VMObjectPaged::new(zbi_data.len() / PAGE_SIZE + 1);
+        vmo.write(0, &zbi_data);
+        vmo
+    };
+
+    let (user_channel, kernel_channel) = Channel::create();
+    let handle = Handle::new(user_channel, Rights::DEFAULT_CHANNEL);
+
+    // FIXME: pass correct handles
+    let mut handles = vec![Handle::new(proc.clone(), Rights::DUPLICATE); 15];
+    handles[2] = Handle::new(job, Rights::DEFAULT_JOB);
+    handles[3] = Handle::new(resource, Rights::DEFAULT_RESOURCE);
+    handles[4] = Handle::new(zbi_vmo, Rights::DEFAULT_VMO);
+    handles[5] = Handle::new(vdso_vmo, Rights::DEFAULT_VMO);
+
+    let mut data = Vec::from(cmdline);
+    data.push(0);
+    let msg = MessagePacket { data, handles };
+    kernel_channel.write(msg).unwrap();
+
+    const STACK_SIZE: usize = 0x8000;
+    let stack = Vec::<u8>::with_capacity(STACK_SIZE);
+    let sp = stack.as_ptr() as usize + STACK_SIZE;
+    proc.start(&thread, entry, sp, handle, 0)
+        .expect("failed to start main thread");
+}
+
+#[naked]
+unsafe fn syscall_entry() {
+    asm!("push rax" :::: "intel");
+    #[cfg(not(target_os = "macos"))]
+    asm!("call handle_syscall" :::: "intel");
+    #[cfg(target_os = "macos")]
+    asm!("call _handle_syscall" :::: "intel");
+    asm!("add rsp, 8" :::: "intel");
+}
+
+#[no_mangle]
+extern "C" fn handle_syscall(
+    a0: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+    num: u32, // pushed %eax
+    _: usize, // return address
+    a6: usize,
+    a7: usize,
+) -> isize {
+    let syscall = Syscall {
+        thread: Thread::current(),
+    };
+    syscall.syscall(num, [a0, a1, a2, a3, a4, a5, a6, a7])
+}
 
 pub trait ElfExt {
     fn load_segment_size(&self) -> usize;
--- zircon-userboot/src/main.rs
+++ zircon-userboot/src/main.rs
@@ -1,133 +1,62 @@
-#![feature(asm)]
-#![feature(naked_functions)]
 #![deny(warnings, unused_must_use)]
-
-#[macro_use]
-extern crate alloc;
 
 extern crate log;
 
-use {
-    alloc::vec::Vec,
-    xmas_elf::ElfFile,
-    zircon_object::{
-        ipc::channel::*,
-        object::*,
-        resource::{Resource, ResourceKind},
-        task::*,
-        vm::*,
-    },
-    zircon_syscall::Syscall,
-    zircon_userboot::*,
-};
+use {std::path::PathBuf, structopt::StructOpt, zircon_userboot::*};
+
+#[derive(Debug, StructOpt)]
+#[structopt()]
+struct Opt {
+    #[structopt(parse(from_os_str))]
+    userboot_path: PathBuf,
+
+    #[structopt(parse(from_os_str))]
+    vdso_path: PathBuf,
+
+    #[structopt(parse(from_os_str))]
+    zbi_path: PathBuf,
+
+    #[structopt(default_value = "")]
+    cmdline: String,
+}
 
 fn main() {
     zircon_hal_unix::init();
     env_logger::init();
 
-    let vmar = VmAddressRegion::new_root();
-    const VBASE: usize = 0x400000000;
+    let opt = Opt::from_args();
+    let userboot_data = std::fs::read(opt.userboot_path).expect("failed to read file");
+    let vdso_data = std::fs::read(opt.vdso_path).expect("failed to read file");
+    let zbi_data = std::fs::read(opt.zbi_path).expect("failed to read file");
 
-    // userboot
-    let (entry, userboot_size) = {
-        let path = std::env::args()
-            .nth(1)
-            .expect("failed to read userboot path");
-        let elf_data = std::fs::read(path).expect("failed to read file");
-        let elf = ElfFile::new(&elf_data).unwrap();
-        let size = elf.load_segment_size();
-        let vmar = vmar.create_child(VBASE, size).unwrap();
-        vmar.load_from_elf(&elf).unwrap();
-        (VBASE + elf.header.pt2.entry_point() as usize, size)
-    };
-
-    // vdso
-    let vdso_vmo = {
-        let path = std::env::args().nth(2).expect("failed to read vdso path");
-        let elf_data = std::fs::read(path).expect("failed to read file");
-        let elf = ElfFile::new(&elf_data).unwrap();
-        let size = elf.load_segment_size();
-        let syscall_entry_offset = elf
-            .get_symbol_address("zcore_syscall_entry")
-            .expect("failed to locate syscall entry") as usize;
-        let vmar = vmar.create_child(VBASE + userboot_size, size).unwrap();
-        let first_vmo = vmar.load_from_elf(&elf).unwrap();
-
-        unsafe {
-            // fill syscall entry
-            ((VBASE + userboot_size + syscall_entry_offset) as *mut usize)
-                .write(syscall_entry as usize);
-        }
-        first_vmo
-    };
-
-    // zbi
-    let zbi_vmo = {
-        let path = std::env::args().nth(3).expect("failed to read zbi path");
-        let zbi_data = std::fs::read(path).expect("failed to read file");
-        let vmo = VMObjectPaged::new(zbi_data.len() / PAGE_SIZE + 1);
-        vmo.write(0, &zbi_data);
-        vmo
-    };
-
-    let job = Job::root();
-    let proc = Process::create(&job, "proc", 0).unwrap();
-    let thread = Thread::create(&proc, "thread", 0).unwrap();
-    let resource = Resource::create("root", ResourceKind::ROOT).unwrap();
-
-    let (user_channel, kernel_channel) = Channel::create();
-    let handle = Handle::new(user_channel, Rights::DEFAULT_CHANNEL);
-    let cmdline = "\0";
-
-    // FIXME: pass correct handles
-    let mut handles = vec![Handle::new(proc.clone(), Rights::DUPLICATE); 15];
-    handles[2] = Handle::new(job, Rights::DEFAULT_JOB);
-    handles[3] = Handle::new(resource, Rights::DEFAULT_RESOURCE);
-    handles[4] = Handle::new(zbi_vmo, Rights::DEFAULT_VMO);
-    handles[5] = Handle::new(vdso_vmo, Rights::DEFAULT_VMO);
-
-    let msg = MessagePacket {
-        data: Vec::from(cmdline),
-        handles,
-    };
-    kernel_channel.write(msg).unwrap();
-
-    const STACK_SIZE: usize = 0x8000;
-    let stack = Vec::<u8>::with_capacity(STACK_SIZE);
-    let sp = stack.as_ptr() as usize + STACK_SIZE;
-    proc.start(&thread, entry, sp, handle, 0)
-        .expect("failed to start main thread");
+    run_userboot(&userboot_data, &vdso_data, &zbi_data, &opt.cmdline);
 
     loop {
         std::thread::park();
     }
 }
 
-#[naked]
-unsafe fn syscall_entry() {
-    asm!("push rax" :::: "intel");
-    #[cfg(not(target_os = "macos"))]
-    asm!("call handle_syscall" :::: "intel");
-    #[cfg(target_os = "macos")]
-    asm!("call _handle_syscall" :::: "intel");
-    asm!("add rsp, 8" :::: "intel");
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[no_mangle]
-extern "C" fn handle_syscall(
-    a0: usize,
-    a1: usize,
-    a2: usize,
-    a3: usize,
-    a4: usize,
-    a5: usize,
-    num: u32, // pushed %eax
-    _: usize, // return address
-    a6: usize,
-    a7: usize,
-) -> isize {
-    let syscall = Syscall {
-        thread: Thread::current(),
-    };
-    syscall.syscall(num, [a0, a1, a2, a3, a4, a5, a6, a7])
+    #[test]
+    #[ignore]
+    fn userboot() {
+        zircon_hal_unix::init();
+
+        let base = PathBuf::from("../prebuilt");
+        let opt = Opt {
+            userboot_path: base.join("userboot.so"),
+            vdso_path: base.join("libzircon.so"),
+            zbi_path: base.join("legacy-image-x64.zbi"),
+            cmdline: String::from(""),
+        };
+        let userboot_data = std::fs::read(opt.userboot_path).expect("failed to read file");
+        let vdso_data = std::fs::read(opt.vdso_path).expect("failed to read file");
+        let zbi_data = std::fs::read(opt.zbi_path).expect("failed to read file");
+
+        run_userboot(&userboot_data, &vdso_data, &zbi_data, &opt.cmdline);
+        unimplemented!("join userboot thread")
+    }
 }
