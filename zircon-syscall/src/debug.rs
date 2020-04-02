edef5d8f1741008c8c5ab6a8681aae58c531229b
PanQL <panqinglin00@163.com>
2020-04-02 19:14:19 +0800

    add naive debug_read

--- zircon-syscall/src/debug.rs
+++ zircon-syscall/src/debug.rs
@@ -7,4 +7,20 @@ impl Syscall<'_> {
         kernel_hal::serial_write("\n");
         Ok(())
     }
+
+    pub fn sys_debug_read(
+        &self,
+        handle: HandleValue,
+        mut buf: UserOutPtr<u8>,
+        buf_size: u32,
+        mut actual: UserOutPtr<u32>,
+    ) -> ZxResult {
+        info!(
+            "handle = {:#x}, buf_size={:#x}",
+            handle, buf_size
+        );
+        buf.write_cstring("HelloWorld\n")?;
+        actual.write(11)?;
+        Ok(())
+    }
 }
--- zircon-syscall/src/lib.rs
+++ zircon-syscall/src/lib.rs
@@ -211,8 +211,9 @@ impl Syscall<'_> {
             ),
             Sys::SYSTEM_GET_EVENT => self.sys_system_get_event(a0 as _, a1 as _, a2.into()),
             Sys::TIMER_SET => self.sys_timer_set(a0 as _, a1.into(), a2 as _),
+            Sys::DEBUG_READ => self.sys_debug_read(a0 as _, a1.into(), a2 as _, a3.into()),
             _ => {
-                warn!("syscall unimplemented: {:?}", sys_type);
+                error!("syscall unimplemented: {:?}", sys_type);
                 Err(ZxError::NOT_SUPPORTED)
             }
         };
