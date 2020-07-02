d2091aef94a186c796e12a3e45adebd128b9856b
Yuekai Jia <equation618@gmail.com>
2020-07-02 14:47:33 +0800

    Update Fuchsia version to 20200630 (#89)
    * Rebuild x64/arm64 prebuilt images & libraries.
    * Add hypervisor test.
    * Update `gen-prebuilt.sh`.
    * Two core test cases are no longoer passed, remove them from `test-check-passed.txt`.

--- scripts/gen-prebuilt.sh
+++ scripts/gen-prebuilt.sh
@@ -1,26 +1,28 @@
 # Generate prebuilt/zircon from modified fuchsia source
 
 OUTDIR=zcore_prebuilt
+ARCH=${1:-x64}
 mkdir -p ${OUTDIR}
 
 # set build target
-./scripts/fx set bringup.x64 --with-base //garnet/packages/tests:zircon --with //src/tests/microbenchmarks
+./scripts/fx set bringup.${ARCH} --with-base //garnet/packages/tests:zircon --with //src/tests/microbenchmarks --with //src/virtualization/tests:hypervisor_tests_pkg
 
 # apply zircon-libos.patch and build once
 patch -p1 < zircon-libos.patch
 ./scripts/fx build default.zircon
 patch -p1 -R < zircon-libos.patch
-cp out/default.zircon/userboot-x64-clang/obj/kernel/lib/userabi/userboot/userboot.so ${OUTDIR}/userboot-libos.so
-cp out/default.zircon/user.vdso-x64-clang.shlib/obj/system/ulib/zircon/libzircon.so.debug ${OUTDIR}/libzircon-libos.so
+cp out/default.zircon/userboot-${ARCH}-clang/obj/kernel/lib/userabi/userboot/userboot.so ${OUTDIR}/userboot-libos.so
+cp out/default.zircon/user.vdso-${ARCH}-clang.shlib/obj/system/ulib/zircon/libzircon.so.debug ${OUTDIR}/libzircon-libos.so
 
 # apply zcore.patch and build again
 patch -p1 < zcore.patch
 ./scripts/fx build
 patch -p1 -R < zcore.patch
-cp out/default.zircon/userboot-x64-clang/obj/kernel/lib/userabi/userboot/userboot.so ${OUTDIR}
-cp out/default.zircon/user.vdso-x64-clang.shlib/obj/system/ulib/zircon/libzircon.so.debug ${OUTDIR}/libzircon.so
-cp out/default.zircon/userboot-x64-clang/obj/system/ulib/hermetic-decompressor/decompress-zstd.so ${OUTDIR}
+cp out/default.zircon/userboot-${ARCH}-clang/obj/kernel/lib/userabi/userboot/userboot.so ${OUTDIR}
+cp out/default.zircon/user.vdso-${ARCH}-clang.shlib/obj/system/ulib/zircon/libzircon.so.debug ${OUTDIR}/libzircon.so
 cp out/default/bringup.zbi ${OUTDIR}
+cp out/default/obj/zircon/system/utest/core/core-tests.zbi ${OUTDIR}
+
 # remove kernel and cmdline from zbi
 cd ${OUTDIR}
 ../out/default.zircon/tools/zbi -x bringup.zbi -D bootfs
