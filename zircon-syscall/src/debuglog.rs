1cbd46e1daef7564a118c10bae0f17be7e645cd2
Runji Wang <wangrunji0408@163.com>
2020-07-07 18:18:18 +0800

    simplify DebugLog and implement sys_debuglog_read
    pass DebugLogTest.WriteRead
    `dlog` command is available

--- zircon-syscall/src/debuglog.rs
+++ zircon-syscall/src/debuglog.rs
@@ -1,9 +1,7 @@
 use {
     super::*,
-    zircon_object::{debuglog::DebugLog, dev::*},
+    zircon_object::{debuglog::*, dev::*},
 };
-
-const FLAG_READABLE: u32 = 0x4000_0000u32;
 
 impl Syscall<'_> {
     pub fn sys_debuglog_create(
@@ -22,6 +20,7 @@ impl Syscall<'_> {
                 .validate(ResourceKind::ROOT)?;
         }
         let dlog = DebugLog::create(options);
+        const FLAG_READABLE: u32 = 0x4000_0000u32;
         let dlog_right = if options & FLAG_READABLE == 0 {
             Rights::DEFAULT_DEBUGLOG
         } else {
@@ -35,22 +34,55 @@ impl Syscall<'_> {
     pub fn sys_debuglog_write(
         &self,
         handle_value: HandleValue,
-        flags: u32,
+        options: u32,
         buf: UserInPtr<u8>,
         len: usize,
     ) -> ZxResult {
         info!(
-            "debuglog.write: handle={:#x?}, flags={:#x?}, buf=({:#x?}; {:#x?})",
-            handle_value, flags, buf, len,
+            "debuglog.write: handle={:#x?}, options={:#x?}, buf=({:#x?}; {:#x?})",
+            handle_value, options, buf, len,
         );
+        const LOG_FLAGS_MASK: u32 = 0x10;
+        if options & !LOG_FLAGS_MASK != 0 {
+            return Err(ZxError::INVALID_ARGS);
+        }
         let datalen = len.min(224);
         let data = buf.read_string(datalen as usize)?;
-        let thread = &self.thread;
-        let proc = thread.proc();
-        let tid = thread.id();
-        let pid = proc.id();
-        proc.get_object_with_rights::<DebugLog>(handle_value, Rights::WRITE)?
-            .write(flags, &data, tid, pid)?;
+        let proc = self.thread.proc();
+        let dlog = proc.get_object_with_rights::<DebugLog>(handle_value, Rights::WRITE)?;
+        dlog.write(Severity::Info, options, self.thread.id(), proc.id(), &data);
+        // print to kernel console
+        kernel_hal::serial_write(&data);
+        if data.as_bytes().last() != Some(&b'\n') {
+            kernel_hal::serial_write("\n");
+        }
         Ok(())
+    }
+
+    #[allow(unsafe_code)]
+    pub fn sys_debuglog_read(
+        &self,
+        handle_value: HandleValue,
+        options: u32,
+        mut buf: UserOutPtr<u8>,
+        len: usize,
+    ) -> ZxResult {
+        info!(
+            "debuglog.read: handle={:#x?}, options={:#x?}, buf=({:#x?}; {:#x?})",
+            handle_value, options, buf, len,
+        );
+        if options != 0 {
+            return Err(ZxError::INVALID_ARGS);
+        }
+        let proc = self.thread.proc();
+        let mut buffer = [0; DLOG_MAX_LEN];
+        let dlog = proc.get_object_with_rights::<DebugLog>(handle_value, Rights::READ)?;
+        let actual_len = dlog.read(&mut buffer).min(len);
+        if actual_len == 0 {
+            return Err(ZxError::SHOULD_WAIT);
+        }
+        buf.write_array(&buffer[..actual_len])?;
+        // special case: return actual_len as status
+        Err(unsafe { core::mem::transmute(actual_len as u32) })
     }
 }
