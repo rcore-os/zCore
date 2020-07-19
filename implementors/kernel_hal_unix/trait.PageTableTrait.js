474474004dbdcf97dc1a6648eb189e31c7e06385
wangrunji0408 <wangrunji0408@163.com>
2020-07-19 04:23:43 +0000

    Deploying to gh-pages from  @ 9900bf1c5f1f5c92b190a9ac5e1ab62c62de9570 🚀

---
+++ implementors/kernel_hal_unix/trait.PageTableTrait.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["kernel_hal_unix"] = [{"text":"impl <a class=\"trait\" href=\"kernel_hal_unix/trait.PageTableTrait.html\" title=\"trait kernel_hal_unix::PageTableTrait\">PageTableTrait</a> for <a class=\"struct\" href=\"kernel_hal_unix/struct.PageTable.html\" title=\"struct kernel_hal_unix::PageTable\">PageTable</a>","synthetic":false,"types":["kernel_hal_unix::PageTable"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
