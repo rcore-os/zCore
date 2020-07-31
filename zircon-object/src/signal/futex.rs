bfce2b178ab5f63a35c4f4ecb42710639a81cc77
xxchan <xxchan@sjtu.edu.cn>
2020-07-31 16:50:27 +0800

    add some docs and tests for `futex`

--- zircon-object/src/signal/futex.rs
+++ zircon-object/src/signal/futex.rs
@@ -33,6 +33,13 @@ struct FutexInner {
 
 impl Futex {
     /// Create a new Futex.
+    ///
+    /// The parameter `value` is the reference to
+    /// an userspace `AtomicI32`. This reference is the
+    /// information used in kernel to track what futex given threads are
+    /// waiting on. The kernel does not currently modify the value of
+    /// `*value`. It is up to userspace code to correctly atomically modify this
+    /// value across threads in order to build mutexes and so on.
     pub fn new(value: &'static AtomicI32) -> Arc<Self> {
         Arc::new(Futex {
             base: KObjectBase::default(),
@@ -295,6 +302,7 @@ impl Waiter {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::task::{Job, Process, ThreadState};
     use core::time::Duration;
 
     #[async_std::test]
@@ -321,5 +329,97 @@ mod tests {
         // wait for wake.
         futex.wait(1).await.unwrap();
         assert_eq!(VALUE.load(Ordering::SeqCst), 2);
+        assert_eq!(futex.wake(1), 0);
+    }
+
+    #[async_std::test]
+    async fn requeue() {
+        static VALUE: AtomicI32 = AtomicI32::new(1);
+        let futex = Futex::new(&VALUE);
+        static REQUEUE_VALUE: AtomicI32 = AtomicI32::new(100);
+        let requeue_futex = Futex::new(&REQUEUE_VALUE);
+
+        let count = futex.wake(1);
+        assert_eq!(count, 0);
+
+        // inconsistent value should fail.
+        assert_eq!(futex.wait(0).await, Err(ZxError::BAD_STATE));
+
+        // spawn a new task to wait
+        {
+            let futex = futex.clone();
+            async_std::task::spawn(async move {
+                futex.wait(1).await.unwrap();
+            });
+        }
+        // spawn a new task to requeue.
+        {
+            let futex = futex.clone();
+            async_std::task::spawn(async move {
+                async_std::task::sleep(Duration::from_millis(10)).await;
+                VALUE.store(2, Ordering::SeqCst);
+                // inconsistent value should fail.
+                assert_eq!(
+                    futex.requeue(1, 1, 1, &requeue_futex, None),
+                    Err(ZxError::BAD_STATE)
+                );
+                assert!(futex.requeue(2, 1, 1, &requeue_futex, None).is_ok());
+            });
+        }
+        // wait for wake.
+        futex.wait(1).await.unwrap();
+        assert_eq!(VALUE.load(Ordering::SeqCst), 2);
+    }
+
+    #[async_std::test]
+    async fn owner() {
+        let root_job = Job::root();
+        let proc = Process::create(&root_job, "proc", 0).expect("failed to create process");
+        let thread = Thread::create(&proc, "thread", 0).expect("failed to create thread");
+
+        static VALUE: AtomicI32 = AtomicI32::new(1);
+        let futex = proc.get_futex(&VALUE);
+        assert!(futex.owner().is_none());
+        futex.inner.lock().set_owner(Some(thread.clone()));
+
+        {
+            let futex = futex.clone();
+            let thread = thread.clone();
+            async_std::task::spawn(async move {
+                futex
+                    .wait_with_owner(1, Some(thread.clone()), Some(thread))
+                    .await
+                    .unwrap();
+            });
+        }
+        async_std::task::sleep(Duration::from_millis(10)).await;
+        assert_eq!(
+            futex
+                .wait_with_owner(1, Some(thread.clone()), Some(thread.clone()))
+                .await
+                .unwrap_err(),
+            ZxError::INVALID_ARGS
+        );
+
+        futex.inner.lock().set_owner(None);
+        futex.wake_single_owner();
+        assert!(Arc::ptr_eq(&futex.owner().unwrap(), &thread));
+        assert_eq!(futex.wake(1), 0);
+    }
+
+    #[async_std::test]
+    async fn time_out() {
+        let root_job = Job::root();
+        let proc = Process::create(&root_job, "proc", 0).expect("failed to create process");
+        let thread = Thread::create(&proc, "thread", 0).expect("failed to create thread");
+
+        static VALUE: AtomicI32 = AtomicI32::new(1);
+        let futex = proc.get_futex(&VALUE);
+        let future = futex.wait_with_owner(1, Some(thread.clone()), Some(thread.clone()));
+        let result: ZxResult = thread
+            .blocking_run(future, ThreadState::BlockedFutex, Duration::from_millis(1))
+            .await;
+        assert_eq!(result.unwrap_err(), ZxError::TIMED_OUT);
+        assert_eq!(futex.wake(1), 0);
     }
 }
