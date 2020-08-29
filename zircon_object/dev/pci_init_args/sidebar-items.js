2b76a97a0832c0500ce09c4d3729783e2cc782bf
wangrunji0408 <wrj19@mails.tsinghua.edu.cn>
2020-08-29 15:59:24 +0000

    Deploying to gh-pages from  @ 6f6f71e43b0598f431809fd7b8f28f99ea0b0e40 🚀

---
+++ zircon_object/dev/pci_init_args/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["PCI_INIT_ARG_MAX_ECAM_WINDOWS",""],["PCI_INIT_ARG_MAX_SIZE",""]],"struct":[["PciInitArgsAddrWindows",""],["PciInitArgsHeader",""],["PciInitArgsIrqs",""],["PciIrqSwizzleLut",""]]});
