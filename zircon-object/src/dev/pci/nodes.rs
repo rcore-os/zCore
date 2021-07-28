453c58d340d468ee0cec457c18a0a854a8d5c184
DeathWish5 <zyr_ms@outlook.com>
2021-07-28 20:08:04 +0800

    Remove an unsafe code

--- zircon-object/src/dev/pci/nodes.rs
+++ zircon-object/src/dev/pci/nodes.rs
@@ -33,23 +33,21 @@ pub struct PcieUpstream {
 
 struct PcieUpstreamInner {
     weak_super: Weak<dyn IPciNode>,
-    downstream: [Option<Arc<dyn IPciNode>>; PCI_MAX_FUNCTIONS_PER_BUS],
+    downstream: Box<[Option<Arc<dyn IPciNode>>]>,
 }
 
 impl PcieUpstream {
-    #[allow(unsafe_code)]
     pub fn create(managed_bus_id: usize) -> Arc<Self> {
         Arc::new(PcieUpstream {
             managed_bus_id,
             inner: Mutex::new(PcieUpstreamInner {
                 weak_super: Weak::<PciRoot>::new(),
-                downstream: unsafe {
-                    core::mem::transmute(
-                        [0u8; core::mem::size_of::<Option<Arc<dyn IPciNode>>>()
-                            * PCI_MAX_FUNCTIONS_PER_BUS],
-                    )
+                downstream: {
+                    let mut vec =
+                        Vec::<Option<Arc<dyn IPciNode>>>::with_capacity(PCI_MAX_FUNCTIONS_PER_BUS);
+                    vec.resize(PCI_MAX_FUNCTIONS_PER_BUS, None);
+                    vec.into_boxed_slice()
                 },
-                // [None; PCI_MAX_FUNCTIONS_PER_BUS],
             }),
         })
     }
