7d9c99e6df0b9e9fc3c40d98de3a4d4bc358be96
shzhxh <shzhxh@163.com>
2021-08-27 09:05:28 +0800

    add core test CI

--- scripts/baremetal-core-tests.py
+++ scripts/baremetal-core-tests.py
@@ -1,14 +1,16 @@
 import pexpect
 import sys
 import re
+import os
 
-TIMEOUT = 300
+TIMEOUT = 20
 ZCORE_PATH = '../zCore'
 BASE = 'zircon/'
-OUTPUT_FILE = BASE + 'test-output.txt'
-RESULT_FILE = BASE + 'test-result.txt'
+OUTPUT_FILE = BASE + 'test-output-baremetal.txt'
+RESULT_FILE = BASE + 'test-result-baremetal.txt'
 CHECK_FILE = BASE + 'test-check-passed.txt'
-TEST_CASE_FILE = BASE + 'testcases.txt'
+DBG_FILE = BASE + 'dbg-b.txt'
+TEST_CASE_FILE = BASE + 'testcases-all.txt'
 
 
 class Tee:
@@ -28,20 +30,22 @@ class Tee:
     def flush(self):
         self.file.flush()
 
+if os.path.exists(OUTPUT_FILE): os.remove(OUTPUT_FILE)
+if os.path.exists(RESULT_FILE): os.remove(RESULT_FILE)
+if os.path.exists(DBG_FILE): os.remove(DBG_FILE)
 
-with open(TEST_CASE_FILE, "r") as f:
-    lines = f.readlines()
-    positive = [line for line in lines if not line.startswith('-')]
-    negative = [line[1:] for line in lines if line.startswith('-')]
-    test_filter = (','.join(positive) + ((',-' + ','.join(negative) if len(negative) > 0 else "") )).replace('\n', '')
+with open(TEST_CASE_FILE, "r") as tcf:
+    lines = tcf.readlines()
+    for line in lines:
+        with open(DBG_FILE, "a") as dbg: print(line, file=dbg)
 
-child = pexpect.spawn("make -C %s test mode=release test_filter='%s'" % (ZCORE_PATH, test_filter),
-                      timeout=TIMEOUT, encoding='utf-8')
-child.logfile = Tee(OUTPUT_FILE, 'w')
+        child = pexpect.spawn("make -C %s test mode=release test_filter='%s'" % (ZCORE_PATH, line.replace('\n','')),
+                            timeout=TIMEOUT, encoding='utf-8')
+        child.logfile = Tee(OUTPUT_FILE, 'a')
 
-index = child.expect(['finished!', 'panicked', pexpect.EOF, pexpect.TIMEOUT])
-result = ['FINISHED', 'PANICKED', 'EOF', 'TIMEOUT'][index]
-print(result)
+        index = child.expect(['finished!', 'panicked', pexpect.EOF, pexpect.TIMEOUT])
+        result = ['FINISHED', 'PANICKED', 'EOF', 'TIMEOUT'][index]
+        print(result)
 
 passed = []
 failed = []
@@ -50,27 +54,24 @@ passed_case = set()
 # see https://stackoverflow.com/questions/59379174/ignore-ansi-colors-in-pexpect-response
 ansi_escape = re.compile(r"\x1B[@-_][0-?]*[ -/]*[@-~]")
 
-with open(OUTPUT_FILE, "r") as f:
-    for line in f.readlines():
+with open(OUTPUT_FILE, "r") as opf:
+    for line in opf:
         line=ansi_escape.sub('',line)
-        if line.startswith('[       OK ]'):
-            passed += line
-            passed_case.add(line[13:].split(' ')[0])
-        elif line.startswith('[  FAILED  ]') and line.endswith(')\n'):
-            failed += line
+        with open(RESULT_FILE, "a") as rstf:
+            if line.startswith('[       OK ]'):
+                print(line, file=rstf)
+            elif line.startswith('[  FAILED  ]') and line.endswith(')\n'):
+                print(line, file=rstf)
 
-with open(RESULT_FILE, "w") as f:
-    f.writelines(passed)
-    f.writelines(failed)
 
-with open(CHECK_FILE, 'r') as f:
-    check_case = set([case.strip() for case in f.readlines()])
+# with open(CHECK_FILE, 'r') as f:
+#     check_case = set([case.strip() for case in f.readlines()])
 
-not_passed = check_case - passed_case
-if not_passed:
-    print('=== Failed cases ===')
-    for case in not_passed:
-        print(case)
-    exit(1)
-else:
-    print('All checked case passed!')
+# not_passed = check_case - passed_case
+# if not_passed:
+#     print('=== Failed cases ===')
+#     for case in not_passed:
+#         print(case)
+#     exit(1)
+# else:
+#     print('All checked case passed!')
--- scripts/unix-core-testone.py
+++ scripts/unix-core-testone.py
@@ -4,8 +4,8 @@ import re
 import argparse
 
 TIMEOUT = 300
-ZIRCON_LOADER_PATH = '../zircon-loader'
-PREBUILT_PATH = '/prebuilt/zircon/x64'
+ZIRCON_LOADER_PATH = 'zircon-loader'
+PREBUILT_PATH = '../prebuilt/zircon/x64'
 CMDLINE_BASE = 'LOG=warn:userboot=test/core-standalone-test:userboot.shutdown:core-tests='
 
 parser = argparse.ArgumentParser()
@@ -13,7 +13,7 @@ parser.add_argument('testcase', nargs=1)
 args = parser.parse_args()
 
 child = pexpect.spawn("cargo run -p '%s' -- '%s' '%s' --debug " % 
-                        (ZIRCON_LOADER_PATH, PREBUILT_PATH, CMDLINE_BASE + args.testcase), 
+                        (ZIRCON_LOADER_PATH, PREBUILT_PATH, CMDLINE_BASE+args.testcase[0]), 
                         timeout=TIMEOUT, encoding='utf-8')
 
 child.logfile = sys.stdout
