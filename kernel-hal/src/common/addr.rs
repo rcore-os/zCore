266e0a68b202dfa45a6f9593ff217016e0e384e6
shzhxh <shzhxh@163.com>
2022-03-15 17:38:47 +0800

    update doc of kernel-hal

--- kernel-hal/src/common/addr.rs
+++ kernel-hal/src/common/addr.rs
@@ -11,22 +11,27 @@ pub type VirtAddr = usize;
 /// Device address.
 pub type DevVAddr = usize;
 
+/// Returns the address of the nearest page that is not larger than the given address.
 pub const fn align_down(addr: usize) -> usize {
     addr & !(PAGE_SIZE - 1)
 }
 
+/// Returns the address of the nearest page whose address is not smaller than the given one.
 pub const fn align_up(addr: usize) -> usize {
     (addr + PAGE_SIZE - 1) & !(PAGE_SIZE - 1)
 }
 
+/// Returns true if the given address is aligned with the page, else false.
 pub const fn is_aligned(addr: usize) -> bool {
     page_offset(addr) == 0
 }
 
+/// The page number of the page where the given address resides.
 pub const fn page_count(size: usize) -> usize {
     align_up(size) / PAGE_SIZE
 }
 
+/// Returns the offset address of given address within a page.
 pub const fn page_offset(addr: usize) -> usize {
     addr & (PAGE_SIZE - 1)
 }
