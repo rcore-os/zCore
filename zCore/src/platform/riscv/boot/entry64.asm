0a4f6dbf388b4a9a5d5738b5872339abcd032dff
YdrMaster <ydrml@hotmail.com>
2022-04-20 04:53:04 +0800

    refactor(zCore): 整理 riscv 启动流程

--- zCore/src/platform/riscv/boot/entry64.asm
+++ zCore/src/platform/riscv/boot/entry64.asm
@@ -1,80 +1,116 @@
-	.section .text.entry
-	.globl _start
-_start:
-	#关中断
-	csrw sie, zero
+# 提供的全局符号
+# --------------------------------------
+# 主核入口
+# fn _start(hartid: usize, device_tree_paddr: usize) -> !;
+    .globl _start
+# 副核入口
+# fn _secondary_hart_start(hartid: usize) -> !;
+    .globl _secondary_hart_start
+# 启动栈（160 页）
+# RISC-V 架构栈从高地址向低地址增长
+# const bootstack:  *const u8; # 栈底
+# const bootstacktop: *mut u8; # 栈顶
+    .global bootstack
+    .global bootstacktop
 
-	#关闭mmu
-	#csrw satp, zero
-	#BSS节清零
-	la t0, sbss
-	la t1, ebss
-	bgeu t0, t1, secondary_hart_start
+# 依赖的全局符号
+# --------------------------------------
+# /// 用于启动的 Sv39 1GiB 页表页
+# const boot_page_table_sv39: *const u8;
+#
+# /// 主核启动函数
+# fn primary_rust_main(hartid: usize, device_tree_paddr: usize) -> !;
+#
+# /// 副核启动函数
+# fn secondary_rust_main() -> !;
 
-clear_bss_loop:
-	# sd: store double word (64 bits)
-	sd zero, (t0)
-	addi t0, t0, 8
-	bltu t0, t1, clear_bss_loop
+    .section .text.entry
 
-primary_hart:
-	call init_vm
-	lui t0, %hi(primary_rust_main)
-	addi t0, t0, %lo(primary_rust_main)
-	jr t0
+# 跨页跳转
+# 加载 `symbol` 的地址并转换到高地址映射，然后跳转
+# --------------------------------------
+.macro jump_cross symbol           #
+    lui  t0, %hi(\symbol)          #
+    addi t0, t0, %lo(\symbol)      #
+    jr   t0                        #
+.endm                              #
+# --------------------------------------
 
+# 主核入口
+# 清零 bss 段，构造启动页表，然后跳转到高地址映射的主核主函数
+# --------------------------------------
+_start:                            # fn _start(hartid: usize, device_tree_paddr: usize) -> ! {{
+    csrw sie, zero                 #     $sie = 0; // 关中断
+                                   #     // 清空 bss 段
+    la t0, sbss                    #     $t0 = &sbss;
+    la t1, ebss                    #     $t1 = &ebss;
+                                   #     loop {{
+1:  sd zero, (t0)                  #         *$t0 = 0usize;
+    addi t0, t0, 8                 #         $t0 += 8;
+    bltu t0, t1, 1b                #         if $t0 < $t1 {{ continue; }}
+                                   #     }}
+    call init_vm                   #     init_vm(hartid); // 启动虚存，并做一些其他初始化工作
+    jump_cross primary_rust_main   #     primary_rust_main(hartid, device_tree_paddr)
+                                   # }}
+# --------------------------------------
 
-.globl secondary_hart_start
-secondary_hart_start:
-	csrw sie, zero
-	call init_vm
-	lui t0, %hi(secondary_rust_main)
-	addi t0, t0, %lo(secondary_rust_main)
-	jr t0
+# 副核入口
+# 构造启动页表，然后跳转到高地址映射的副核主函数
+# --------------------------------------
+_secondary_hart_start:             # fn _secondary_hart_start(hartid: usize) -> ! {{
+    csrw sie, zero                 #     $sie = 0;        // 关中断
+    call init_vm                   #     init_vm(hartid); // 启动虚存，并做一些其他初始化工作
+    jump_cross secondary_rust_main #     secondary_rust_main()
+                                   # }}
+# --------------------------------------
 
+# 构造启动页表并启用地址映射
+# --------------------------------------
 init_vm:
-	#la sp, bootstacktop
-	#call rust_main
+    .equ SATP_MODE_SV39, 8 << 60
+    # 可清零低12位地址
+    lui t0, %hi(boot_page_table_sv39)
+    li t1, PHY_MEM_OFS # 立即数加载
+    # 计算出页表的物理地址
+    sub t0, t0, t1
 
-	#可清零低12位地址
-	lui t0, %hi(boot_page_table_sv39)
-	li t1, PHY_MEM_OFS #立即数加载
-	#计算出页表的物理地址
-	sub t0, t0, t1
+    # 右移12位，变为satp的PPN
+    srli t0, t0, 12
 
-	#右移12位，变为satp的PPN
-	srli t0, t0, 12
+    # satp的MODE设为Sv39
+    li t1, SATP_MODE_SV39
+    or t0, t0, t1
+    csrw satp, t0
 
-	#satp的MODE设为Sv39
-	li t1, 8 << 60
+    # 刷新TLB
+    sfence.vma
 
-	#写satp
-	or t0, t0, t1
-	csrw satp, t0
+    # 立即跳到高地址
+    li t1, PHY_MEM_OFS
+    la t0, 1f
+    add t0, t0, t1
+    jr t0
 
-	#刷新TLB
-	sfence.vma
+1:  mv t0, zero
+    mv t1, a0
+    beqz t1, 2f
 
-	# t0 = 0
-	xor t0, t0, t0
-	mv t1, a0
-	beqz t1, 2f
-	li t2, 4096 * 16
-1:
-	add t0, t0, t2
-	addi t1, t1, -1
-	bgtz t1, 1b
-2:
-	#此时在虚拟内存空间，设置sp为虚拟地址
-	lui sp, %hi(bootstacktop)
-	sub sp, sp, t0
-	ret
+    li   t2, 4096 * 16
+1:  add  t0, t0, t2
+    addi t1, t1, -1
+    bgtz t1, 1b
 
-	.section .bss.stack
-	.align 12
-	.global bootstack
+    # 此时在虚拟内存空间，设置 sp 为虚拟地址
+2:  lui sp, %hi(bootstacktop)
+    sub sp, sp, t0
+
+    mv tp, a0
+    csrrsi x0, sstatus, 18
+    ret
+# --------------------------------------
+
+    .section .bss.bootstack
+    .align 12
 bootstack:
-	.space 4096 * 160
-	.global bootstacktop
+    .space 4096 * 160
 bootstacktop:
-
--- zCore/src/platform/riscv/entry.rs
+++ zCore/src/platform/riscv/entry.rs
@@ -10,65 +10,54 @@ global_asm!(
     include_str!("boot/entry64.asm"),
 );
 
-use super::consts::*;
-use core::arch::{asm, global_asm};
-use core::str::FromStr;
-use kernel_hal::arch::sbi::{hart_start, send_ipi, SBI_SUCCESS};
-use kernel_hal::KernelConfig;
-const SMP: &str = core::env!("SMP"); // Get HART number from the environment variable
-
-extern "C" {
-    fn secondary_hart_start();
-}
+use super::consts::PHYSICAL_MEMORY_OFFSET;
+use core::{
+    arch::{asm, global_asm},
+    str::FromStr,
+};
+use kernel_hal::{
+    arch::sbi::{hart_start, send_ipi, SBI_SUCCESS},
+    KernelConfig,
+};
 
 #[no_mangle]
 pub extern "C" fn primary_rust_main(hartid: usize, device_tree_paddr: usize) -> ! {
-    unsafe {
-        asm!("mv tp, {0}", in(reg) hartid);
-        let mut sstatus: usize;
-        asm!("csrr {0}, sstatus", out(reg) sstatus);
-        sstatus |= 1 << 18;
-        asm!("csrw sstatus, {0}", in(reg) sstatus);
-        println!(
-            "boot hart: zCore rust_main(hartid: {}, device_tree_paddr: {:#x}) sstatus={:x}",
-            hartid, device_tree_paddr, sstatus
-        );
-    };
+    let sstatus: usize;
+    unsafe { asm!("csrr {0}, sstatus", out(reg) sstatus) };
+    println!(
+        "boot hart: zCore rust_main(hartid: {}, device_tree_paddr: {:#x}) sstatus={:#x}",
+        hartid, device_tree_paddr, sstatus
+    );
 
-    let config = KernelConfig {
-        phys_to_virt_offset: PHYSICAL_MEMORY_OFFSET,
-        dtb_paddr: device_tree_paddr,
-    };
-    for id in 0..usize::from_str(SMP).expect("can't parse SMP as usize.") {
+    for id in 0..usize::from_str(core::env!("SMP")).expect("can't parse SMP as usize.") {
         if id != hartid {
+            extern "C" {
+                fn _secondary_hart_start();
+            }
             let err_code = hart_start(
                 id,
-                secondary_hart_start as usize - PHYSICAL_MEMORY_OFFSET, // cal physical address
+                _secondary_hart_start as usize - PHYSICAL_MEMORY_OFFSET, // cal physical address
                 0,
             );
             if err_code != SBI_SUCCESS {
                 panic!("start hart{} failed. error code={}", id, err_code);
             }
-            let hart_mask: usize = 1 << id;
+            let hart_mask = 1usize << id;
             let err_code = send_ipi(&hart_mask as *const _ as usize);
             if err_code != SBI_SUCCESS {
                 panic!("send ipi to hart{} failed. error code={}", id, err_code);
             }
         }
     }
-    crate::primary_main(config);
+
+    crate::primary_main(KernelConfig {
+        phys_to_virt_offset: PHYSICAL_MEMORY_OFFSET,
+        dtb_paddr: device_tree_paddr,
+    });
     unreachable!()
 }
 
-// Don't print in this function and use console_write_early if necessary
 #[no_mangle]
-pub extern "C" fn secondary_rust_main(hartid: usize) -> ! {
-    unsafe {
-        asm!("mv tp, {0}", in(reg) hartid);
-        let mut sstatus: usize;
-        asm!("csrr {0}, sstatus", out(reg) sstatus);
-        sstatus |= 1 << 18; // set SUM=1
-        asm!("csrw sstatus, {0}", in(reg) sstatus);
-    };
-    crate::secondary_main();
+pub extern "C" fn secondary_rust_main() -> ! {
+    crate::secondary_main()
 }
