fde18af386440ff0512fa55492c1a534ee5a0ed4
YdrMaster <ydrml@hotmail.com>
2022-04-22 00:29:52 +0800

    refactor(zCore): 启动过程完全 rust 化
    todo: 适配 d1

--- zCore/src/platform/riscv/boot/entry64.asm
+++ zCore/src/platform/riscv/boot/entry64.asm
@@ -1,100 +1,68 @@
 # 提供的全局符号
-# --------------------------------------
-# 主核入口
+# -------------------------------
+# /// 主核入口
 # fn _start(hartid: usize, device_tree_paddr: usize) -> !;
-    .globl _start
-# 副核入口
+    .global _start
+# /// 副核入口
 # fn _secondary_hart_start(hartid: usize) -> !;
-    .globl _secondary_hart_start
-# 启动栈（160 页）
-# RISC-V 架构栈从高地址向低地址增长
-# const bootstack:  *const u8; # 栈底
-# const bootstacktop: *mut u8; # 栈顶
-    .global bootstack
-    .global bootstacktop
+    .global _secondary_hart_start
+# /// 地址空间跃迁
+# fn jump_heigher(offset: usize);
+    .global _jump_higher
 
 # 依赖的全局符号
-# --------------------------------------
-# /// 用于启动的 Sv39 1GiB 页表页
-# const boot_page_table_sv39: *const u8;
-#
-# /// 主核启动函数
+# -------------------------------
+# /// 主核入口
 # fn primary_rust_main(hartid: usize, device_tree_paddr: usize) -> !;
 #
-# /// 副核启动函数
-# fn secondary_rust_main() -> !;
+# /// 副核入口
+# fn secondary_rust_main(hartid: usize) -> !;
 
     .section .text.entry
 
-# 跨页跳转
-# 加载 `symbol` 的地址并转换到高地址映射，然后跳转
-# --------------------------------------
-.macro jump_cross symbol            #
-    lui  t0, %hi(\symbol)           #
-    addi t0, t0, %lo(\symbol)       #
-    jr   t0                         #
-.endm                               #
-# --------------------------------------
-
 # 主核入口
 # 清零 bss 段，构造启动页表，然后跳转到高地址映射的主核主函数
-# --------------------------------------
-_start:                             # fn _start(hartid: usize, device_tree_paddr: usize) -> ! {{
-    csrw sie,  zero                 #     $sie = 0; // 关中断
-                                    #     // 清空 bss 段
-    la   t0,   sbss                 #     $t0 = sbss;
-    la   t1,   ebss                 #     $t1 = ebss;
-                                    #     loop {{
-1:  sd   zero, (t0)                 #         *$t0 = 0usize;
-    addi t0,   t0, 8                #         $t0 += 8;
-    bltu t0,   t1, 1b               #         if $t0 < $t1 {{ continue; }}
-                                    #     }}
-    call       init_vm              #     init_vm(hartid); // 启动虚存，并做一些其他初始化工作
-    jump_cross primary_rust_main    #     primary_rust_main(hartid, device_tree_paddr)
-                                    # }}
-# --------------------------------------
+# -------------------------------
+_start:                      # fn _start(hartid: usize, device_tree_paddr: usize) -> ! {{
+    csrw sie,  zero          #     $sie = 0; // 关中断
+    call select_stack        #     select_stack(hartid);
+    j    primary_rust_main   #     primary_rust_main(hartid, device_tree_paddr)
+                             # }}
+# -------------------------------
 
 # 副核入口
 # 构造启动页表，然后跳转到高地址映射的副核主函数
-# --------------------------------------
-_secondary_hart_start:              # fn _secondary_hart_start(hartid: usize) -> ! {{
-    csrw sie, zero                  #     $sie = 0;        // 关中断
-    call       init_vm              #     init_vm(hartid); // 启动虚存，并做一些其他初始化工作
-    jump_cross secondary_rust_main  #     secondary_rust_main()
-                                    # }}
-# --------------------------------------
+# -------------------------------
+_secondary_hart_start:       # fn _secondary_hart_start(hartid: usize) -> ! {{
+    csrw sie, zero           #     $sie = 0; // 关中断
+    call select_stack        #     select_stack(hartid);
+    j    secondary_rust_main #     secondary_rust_main(hartid)
+                             # }}
+# -------------------------------
 
-# 构造启动页表并启用地址映射
-# --------------------------------------
-init_vm:                            # fn init_vm(hartid: usize) {{
-    .equ SATP_MODE_SV39, 8 << 60    #     const SATP_MODE_SV39: usize = 8 << 60;
-                                    #     // 构造并使能启动页表
-    li   t1,   SATP_MODE_SV39       #     $t1   = SATP_MODE_SV39;
-    la   t0,   boot_page_table_sv39 #     $t0   = boot_page_table_sv39;
-    srli t0,   t0, 12               #     $t0 >>= 12;
-    or   t0,   t0, t1               #     $t0  |= $t1;
-    csrw satp, t0                   #     $satp = $t0;
-    sfence.vma                      #     // 刷新 TLB
-                                    #     // 立即跳到高地址
-    li   t1,   PHY_MEM_OFS          #     $t1   = PHY_MEM_OFS;
-    la   t0,   1f                   #     $t0   = @1f;
-    add  t0,   t0, t1               #     $t0  += $t1;
-    jr   t0                         #     // 跳到高映射的虚地址
-                                    #     // 设置启动栈
-1:  lui  sp,   %hi(bootstacktop)    #     $sp   = bootstacktop;
-    mv   t0,   a0                   #     $t0   = hartid;
-    beqz t0,   2f                   #     if $t0 != 0 {{
-    li   t1,   -4096 * 16           #         $t1  = -4096 * 16;
-                                    #         loop {{
-1:  add  sp,   sp, t1               #             $sp += $t1;
-    addi t0,   t0, -1               #             $t0 -= 1;
-    bgtz t0,   1b                   #             if $t0 > 0 {{ continue; }}
-                                    #         }}
-                                    #     }}
-2:  mv   tp,   a0                   #     // 设置线程指针
-    csrrsi x0, sstatus, 18          #     // 使能内核访问用户页
-    ret                             # }}
-# --------------------------------------
+# 根据线程号设置启动栈
+# -------------------------------
+select_stack:                # fn select_stack(hartid: usize) {{
+    mv   t0, a0              #     $t0 = hartid;
+    la   sp, bootstacktop    #     $sp = bootstacktop;
+    beqz t0, 2f              #     if $t0 != 0 {{
+    li   t1, -4096*16        #         $t1 = -4096*16;
+                             #         loop {{
+1:  add  sp, sp, t1          #             $sp += $t1;
+    addi t0, t0, -1          #             $t0 -= 1;
+    bgtz t0, 1b              #             if $t0 > 0 {{ continue; }}
+                             #         }}
+                             #     }}
+2:  ret                      # }}
+# -------------------------------
+
+# 从地址空间的低处跳到地址空间高处对应位置并挪动栈指针
+# -------------------------------
+_jump_higher:                # fn jump_heigher(offset: usize) {{
+    add  ra, ra, a0          #     $t0 += offset;
+    add  sp, sp, a0          #     $sp += offset;
+    ret                      # }}
+# -------------------------------
 
     .section .bss.bootstack
     .align 12
--- zCore/src/platform/riscv/entry.rs
+++ zCore/src/platform/riscv/entry.rs
@@ -1,63 +1,128 @@
-#[cfg(feature = "board-qemu")]
-global_asm!(
-    include_str!("boot/boot_qemu.asm"),
-    include_str!("boot/entry64.asm"),
-);
-
-#[cfg(feature = "board-d1")]
-global_asm!(
-    include_str!("boot/boot_d1.asm"),
-    include_str!("boot/entry64.asm"),
-);
-
 use super::consts::PHYSICAL_MEMORY_OFFSET;
 use core::{
     arch::{asm, global_asm},
     str::FromStr,
 };
 use kernel_hal::{
-    arch::sbi::{hart_start, send_ipi, SBI_SUCCESS},
+    sbi::{hart_start, send_ipi, shutdown, SBI_SUCCESS},
     KernelConfig,
 };
 
+global_asm!(include_str!("boot/entry64.asm"));
+
+// 启动页表
+#[repr(align(4096))]
+struct BootPageTable([usize; 512]);
+
+static mut BOOT_PAGE_TABLE: BootPageTable = BootPageTable([0; 512]);
+
+// 各级页面容量
+const KIB_BITS: usize = 12; // 4KiB
+const MIB_BITS: usize = KIB_BITS + 9; // 2MiB
+const GIB_BITS: usize = MIB_BITS + 9; // 1GiB
+
+// 各级页号遮罩
+// const KIB_MASK: usize = !((1 << KIB_BITS) - 1);
+// const MIB_MASK: usize = !((1 << MIB_BITS) - 1);
+const GIB_MASK: usize = !((1 << GIB_BITS) - 1);
+
+/// 填充 `satp`
+const MODE_SV39: usize = 8 << 60;
+
+/// 内核页属性
+const DAGXWRV: usize = 0xef;
+
+// 符号表
+extern "C" {
+    /// 内核入口
+    fn _start();
+    /// 副核入口
+    fn _secondary_hart_start();
+    /// 向上跳到距离为 `offset` 的新地址，继续执行
+    fn _jump_higher(offset: usize);
+    /// bss 段起始地址
+    fn sbss();
+    /// bss 段结束地址
+    fn ebss();
+}
+
 #[no_mangle]
 pub extern "C" fn primary_rust_main(hartid: usize, device_tree_paddr: usize) -> ! {
-    let sstatus: usize;
-    unsafe { asm!("csrr {0}, sstatus", out(reg) sstatus) };
-    println!(
-        "boot hart: zCore rust_main(hartid: {}, device_tree_paddr: {:#x}) sstatus={:#x}",
-        hartid, device_tree_paddr, sstatus
-    );
+    // 清零 bss 段
+    let len = (ebss as usize - sbss as usize) / core::mem::size_of::<usize>();
+    unsafe { core::slice::from_raw_parts_mut(sbss as *mut usize, len) }.fill(0);
 
+    // 内核的 GiB 页物理页号
+    let start_ppn = ((_start as usize) & GIB_MASK) >> KIB_BITS;
+    // 内核 GiB 物理页帧在 GiB 页表中的序号
+    let start_pte_index = (_start as usize) >> GIB_BITS;
+    // 容纳内核的页表项
+    let pte = (start_ppn << 10) | DAGXWRV;
+
+    // 构造启动页表
+    unsafe {
+        // 0x0000_0000_8000_0000 -> 0x8000_0000
+        *BOOT_PAGE_TABLE.0.get_unchecked_mut(start_pte_index) = pte;
+        // 0xffff_ffff_8000_0000 -> 0x8000_0000
+        *BOOT_PAGE_TABLE.0.get_unchecked_mut(510) = pte;
+    }
+
+    // 启动副核
     for id in 0..usize::from_str(core::env!("SMP")).expect("can't parse SMP as usize.") {
         if id != hartid {
-            extern "C" {
-                fn _secondary_hart_start();
-            }
-            let err_code = hart_start(
-                id,
-                _secondary_hart_start as usize - PHYSICAL_MEMORY_OFFSET, // cal physical address
-                0,
-            );
+            let err_code = hart_start(id, _secondary_hart_start as _, 0);
             if err_code != SBI_SUCCESS {
                 panic!("start hart{} failed. error code={}", id, err_code);
             }
             let hart_mask = 1usize << id;
-            let err_code = send_ipi(&hart_mask as *const _ as usize);
+            let err_code = send_ipi(&hart_mask as *const _ as _);
             if err_code != SBI_SUCCESS {
                 panic!("send ipi to hart{} failed. error code={}", id, err_code);
             }
         }
     }
 
+    let sstatus = unsafe { BOOT_PAGE_TABLE.launch(hartid) };
+    println!(
+        "
+boot hart: zCore rust_main(hartid: {}, device_tree_paddr: {:#x})
+sstatus = {:#x}",
+        hartid, device_tree_paddr, sstatus
+    );
+
     crate::primary_main(KernelConfig {
         phys_to_virt_offset: PHYSICAL_MEMORY_OFFSET,
         dtb_paddr: device_tree_paddr,
     });
-    unreachable!()
+    shutdown()
 }
 
 #[no_mangle]
-pub extern "C" fn secondary_rust_main() -> ! {
+pub extern "C" fn secondary_rust_main(hartid: usize) -> ! {
+    let _ = unsafe { BOOT_PAGE_TABLE.launch(hartid) };
     crate::secondary_main()
+}
+
+impl BootPageTable {
+    /// 设置启动页表，并跃迁到高地址。
+    ///
+    /// # Safety
+    ///
+    /// 内含极度危险的地址空间跃迁操作，必须内联。
+    #[inline(always)]
+    unsafe fn launch(&self, hartid: usize) -> usize {
+        // 启动页表的页号，将填写到 `satp`
+        let satp = MODE_SV39 | ((self.0.as_ptr() as usize) >> KIB_BITS);
+        // 启动地址转换
+        riscv::register::satp::write(satp);
+        riscv::asm::sfence_vma_all();
+        // 跳到高页面对应位置
+        _jump_higher(PHYSICAL_MEMORY_OFFSET);
+        // 设置线程指针
+        asm!("mv tp, {}", in(reg) hartid);
+        // 设置内核可访问用户页
+        let sstatus: usize;
+        asm!("csrrsi {}, sstatus, 18", out(reg) sstatus);
+        sstatus
+    }
 }
