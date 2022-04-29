4f6e3b2a50189159da918c3c7d1654068bf49072
YdrMaster <ydrml@hotmail.com>
2022-04-29 12:52:40 +0800

    refactor: 启动页表定义分离到独立的文件

--- zCore/src/platform/riscv/entry.rs
+++ zCore/src/platform/riscv/entry.rs
@@ -1,26 +1,9 @@
-use super::consts::PHYSICAL_MEMORY_OFFSET;
+use super::{boot_page_table::BootPageTable, consts::PHYSICAL_MEMORY_OFFSET};
 use core::{arch::asm, str::FromStr};
 use kernel_hal::{
     sbi::{hart_start, send_ipi, shutdown, SBI_SUCCESS},
     KernelConfig,
 };
-
-// 各级页面容量
-const KIB_BITS: usize = 12; // 4KiB
-const MIB_BITS: usize = KIB_BITS + 9; // 2MiB
-const GIB_BITS: usize = MIB_BITS + 9; // 1GiB
-
-// 各级页号遮罩
-// const KIB_MASK: usize = !((1 << KIB_BITS) - 1);
-// const MIB_MASK: usize = !((1 << MIB_BITS) - 1);
-const GIB_MASK: usize = !((1 << GIB_BITS) - 1);
-const SV39_MASK: usize = (1 << (GIB_BITS + 9)) - 1;
-
-/// 填充 `satp`
-const MODE_SV39: usize = 8 << 60;
-
-/// 内核页属性
-const DAGXWRV: usize = 0xef;
 
 /// 内核入口。
 ///
@@ -47,7 +30,7 @@ unsafe extern "C" fn _start(hartid: usize, device_tree_paddr: usize) -> ! {
 ///
 /// 裸函数。
 #[naked]
-unsafe extern "C" fn _secondary_hart_start(hartid: usize) -> ! {
+unsafe extern "C" fn secondary_hart_start(hartid: usize) -> ! {
     asm!(
         "csrw sie, zero",      // 关中断
         "call {select_stack}", // 设置启动栈
@@ -56,6 +39,51 @@ unsafe extern "C" fn _secondary_hart_start(hartid: usize) -> ! {
         main = sym secondary_rust_main,
         options(noreturn)
     )
+}
+
+/// 启动页表
+static mut BOOT_PAGE_TABLE: BootPageTable = BootPageTable::ZERO;
+
+/// 主核启动。
+extern "C" fn primary_rust_main(hartid: usize, device_tree_paddr: usize) -> ! {
+    // 清零 bss 段
+    zero_bss();
+    // 初始化启动页表
+    unsafe { BOOT_PAGE_TABLE.init() };
+    // 启动副核
+    for id in 0..usize::from_str(core::env!("SMP")).expect("can't parse SMP as usize.") {
+        if id != hartid {
+            let err_code = hart_start(id, secondary_hart_start as _, 0);
+            if err_code != SBI_SUCCESS {
+                panic!("start hart{} failed. error code={}", id, err_code);
+            }
+            let hart_mask = 1usize << id;
+            let err_code = send_ipi(&hart_mask as *const _ as _);
+            if err_code != SBI_SUCCESS {
+                panic!("send ipi to hart{} failed. error code={}", id, err_code);
+            }
+        }
+    }
+    // 使能启动页表
+    let sstatus = unsafe { BOOT_PAGE_TABLE.launch(hartid) };
+    println!(
+        "
+boot hart: zCore rust_main(hartid: {}, device_tree_paddr: {:#x})
+sstatus = {:#x}",
+        hartid, device_tree_paddr, sstatus
+    );
+    // 转交控制权
+    crate::primary_main(KernelConfig {
+        phys_to_virt_offset: PHYSICAL_MEMORY_OFFSET,
+        dtb_paddr: device_tree_paddr,
+    });
+    shutdown()
+}
+
+/// 副核启动。
+extern "C" fn secondary_rust_main(hartid: usize) -> ! {
+    let _ = unsafe { BOOT_PAGE_TABLE.launch(hartid) };
+    crate::secondary_main()
 }
 
 /// 根据硬件线程号设置启动栈。
@@ -68,7 +96,7 @@ unsafe extern "C" fn select_stack(hartid: usize) {
     const STACK_PAGES_PER_HART: usize = 16;
     const MAX_HART_NUM: usize = 10;
 
-    const STACK_LEN_PER_HART: usize = (1 << KIB_BITS) * STACK_PAGES_PER_HART;
+    const STACK_LEN_PER_HART: usize = 4096 * STACK_PAGES_PER_HART;
     const STACK_LEN_TOTAL: usize = STACK_LEN_PER_HART * MAX_HART_NUM;
 
     #[link_section = ".bss.bootstack"]
@@ -88,8 +116,9 @@ unsafe extern "C" fn select_stack(hartid: usize) {
     )
 }
 
-/// 主核启动。
-extern "C" fn primary_rust_main(hartid: usize, device_tree_paddr: usize) -> ! {
+/// 清零 bss 段
+#[inline(always)]
+fn zero_bss() {
     #[cfg(target_arch = "riscv32")]
     type Word = u32;
     #[cfg(target_arch = "riscv64")]
@@ -99,107 +128,4 @@ extern "C" fn primary_rust_main(hartid: usize, device_tree_paddr: usize) -> ! {
         static mut ebss: Word;
     }
     unsafe { r0::zero_bss(&mut sbss, &mut ebss) };
-
-    // 内核的 GiB 页物理页号
-    let start_ppn = ((_start as usize) & GIB_MASK) >> KIB_BITS;
-    // 内核 GiB 物理页帧在 GiB 页表中的序号
-
-    let trampoline_pte_index = (_start as usize) >> GIB_BITS;
-    let mut pte_index = (PHYSICAL_MEMORY_OFFSET & SV39_MASK) >> GIB_BITS;
-    // 容纳内核的页表项
-    let pte = (start_ppn << 10) | DAGXWRV;
-
-    // 构造启动页表
-    unsafe {
-        *BOOT_PAGE_TABLE.0.get_unchecked_mut(trampoline_pte_index) = pte;
-        let mut page = DAGXWRV;
-        while pte_index < 512 {
-            *BOOT_PAGE_TABLE.0.get_unchecked_mut(pte_index) = page;
-            page += 1 << (GIB_BITS + 10 - KIB_BITS);
-            pte_index += 1;
-        }
-    }
-
-    // 启动副核
-    for id in 0..usize::from_str(core::env!("SMP")).expect("can't parse SMP as usize.") {
-        if id != hartid {
-            let err_code = hart_start(id, _secondary_hart_start as _, 0);
-            if err_code != SBI_SUCCESS {
-                panic!("start hart{} failed. error code={}", id, err_code);
-            }
-            let hart_mask = 1usize << id;
-            let err_code = send_ipi(&hart_mask as *const _ as _);
-            if err_code != SBI_SUCCESS {
-                panic!("send ipi to hart{} failed. error code={}", id, err_code);
-            }
-        }
-    }
-
-    // 使能启动页表
-    let sstatus = unsafe { BOOT_PAGE_TABLE.launch(hartid) };
-    println!(
-        "
-boot hart: zCore rust_main(hartid: {}, device_tree_paddr: {:#x})
-sstatus = {:#x}",
-        hartid, device_tree_paddr, sstatus
-    );
-
-    crate::primary_main(KernelConfig {
-        phys_to_virt_offset: PHYSICAL_MEMORY_OFFSET,
-        dtb_paddr: device_tree_paddr,
-    });
-    shutdown()
-}
-
-/// 副核启动。
-extern "C" fn secondary_rust_main(hartid: usize) -> ! {
-    let _ = unsafe { BOOT_PAGE_TABLE.launch(hartid) };
-    crate::secondary_main()
-}
-
-/// 启动页表。
-#[repr(align(4096))]
-struct BootPageTable([usize; 512]);
-
-static mut BOOT_PAGE_TABLE: BootPageTable = BootPageTable([0; 512]);
-
-impl BootPageTable {
-    /// 向上跳到距离为 `offset` 的新地址然后继续执行。
-    ///
-    /// # Safety
-    ///
-    /// 裸函数。
-    #[naked]
-    unsafe extern "C" fn jump_higher(offset: usize) {
-        asm!(
-            //
-            "add ra, ra, a0",
-            "add sp, sp, a0",
-            "ret",
-            options(noreturn)
-        )
-    }
-
-    /// 设置启动页表，并跃迁到高地址。
-    ///
-    /// # Safety
-    ///
-    /// 调用前后位于不同的地址空间，必须内联。
-    #[inline(always)]
-    unsafe fn launch(&self, hartid: usize) -> usize {
-        // 启动页表的页号，将填写到 `satp`
-        let satp = MODE_SV39 | ((self.0.as_ptr() as usize) >> KIB_BITS);
-        // 启动地址转换
-        riscv::register::satp::write(satp);
-        // 此时原本的地址空间还在，所以按理说不用刷快表
-        // riscv::asm::sfence_vma_all();
-        // 跳到高页面对应位置
-        Self::jump_higher(PHYSICAL_MEMORY_OFFSET);
-        // 设置线程指针
-        asm!("mv tp, {}", in(reg) hartid);
-        // 设置内核可访问用户页
-        let sstatus: usize;
-        asm!("csrrsi {}, sstatus, 18", out(reg) sstatus);
-        sstatus
-    }
 }
