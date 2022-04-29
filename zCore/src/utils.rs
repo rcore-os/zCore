b15c313075e2df691689a2583978c4ed6b450879
DeathWish5 <zyr_ms@outlook.com>
2022-04-29 16:17:20 +0800

    feat: add rcore-user as linux-user

--- zCore/src/utils.rs
+++ zCore/src/utils.rs
@@ -61,8 +61,6 @@ pub fn boot_options() -> BootOptions {
                 log_level: String::from(*options.get("LOG").unwrap_or(&"")),
                 #[cfg(feature = "linux")]
                 root_proc: String::from(*options.get("ROOTPROC").unwrap_or(&"/bin/busybox?sh")),
-                // root_proc: String::from(*options.get("ROOTPROC")
-                //     .unwrap_or(&"/libc-test/functional/pthread_mutex.exe")),
             }
         }
     }
