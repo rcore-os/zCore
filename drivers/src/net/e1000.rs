19f5cf712f0323f49b962e60bebd994c67a0aea3
Luoyuan Xiao <xiaoluoyuan@163.com>
2022-05-20 14:33:30 +0800

    Add e1000 driver, and some test cases Modify smoltcp reference link

---
+++ drivers/src/net/e1000.rs
@@ -0,0 +1,215 @@
+//! Intel PRO/1000 Network Adapter i.e. e1000 network driver
+//! Datasheet: https://www.intel.ca/content/dam/doc/datasheet/82574l-gbe-controller-datasheet.pdf
+
+use alloc::collections::BTreeMap;
+use alloc::string::String;
+use alloc::sync::Arc;
+use alloc::vec::Vec;
+
+use smoltcp::iface::*;
+use smoltcp::phy::{self, DeviceCapabilities};
+use smoltcp::time::Instant;
+use smoltcp::wire::*;
+use smoltcp::Result;
+
+use super::ProviderImpl;
+use super::PAGE_SIZE;
+use isomorphic_drivers::net::ethernet::intel::e1000::E1000;
+use isomorphic_drivers::net::ethernet::structs::EthernetAddress as DriverEthernetAddress;
+use lock::Mutex;
+
+use crate::net::get_sockets;
+use crate::scheme::{NetScheme, Scheme};
+use crate::{DeviceError, DeviceResult};
+
+#[derive(Clone)]
+pub struct E1000Driver(Arc<Mutex<E1000<ProviderImpl>>>);
+
+pub struct E1000Interface {
+    iface: Mutex<Interface<'static, E1000Driver>>,
+    driver: E1000Driver,
+    name: String,
+    irq: usize,
+}
+
+impl Scheme for E1000Interface {
+    fn name(&self) -> &str {
+        "e1000"
+    }
+
+    fn handle_irq(&self, irq: usize) {
+        if irq != self.irq {
+            // not ours, skip it
+            return;
+        }
+
+        let data = self.driver.0.lock().handle_interrupt();
+
+        if data {
+            //let timestamp = Instant::from_millis(crate::trap::uptime_msec() as i64);
+            // Fix me
+            let timestamp = Instant::from_millis(0);
+            let sockets = get_sockets();
+            let mut sockets = sockets.lock();
+            match self.iface.lock().poll(&mut sockets, timestamp) {
+                Ok(_) => {
+                    //SOCKET_ACTIVITY.notify_all();
+                    info!("e1000 try_handle_interrupt SOCKET_ACTIVITY unimplemented");
+                }
+                Err(err) => {
+                    warn!("poll got err {}", err);
+                }
+            }
+        }
+    }
+}
+
+impl NetScheme for E1000Interface {
+    fn get_mac(&self) -> EthernetAddress {
+        self.iface.lock().ethernet_addr()
+    }
+
+    fn get_ifname(&self) -> String {
+        self.name.clone()
+    }
+
+    // get ip addresses
+    fn get_ip_addrrs(&self) -> Vec<IpCidr> {
+        Vec::from(self.iface.lock().ip_addrs())
+    }
+
+    fn poll(&self) -> DeviceResult {
+        //let timestamp = Instant::from_millis(crate::trap::uptime_msec() as i64);
+        let timestamp = Instant::from_millis(0);
+        let sockets = get_sockets();
+        let mut sockets = sockets.lock();
+        match self.iface.lock().poll(&mut sockets, timestamp) {
+            Ok(p) => {
+                //SOCKET_ACTIVITY.notify_all();
+                info!("e1000 poll SOCKET_ACTIVITY unimplemented.\n{:?}", p);
+                Ok(())
+            }
+            Err(err) => {
+                warn!("poll got err {}", err);
+                Err(DeviceError::IoError)
+            }
+        }
+    }
+
+    fn recv(&self, buf: &mut [u8]) -> DeviceResult<usize> {
+        if let Some(vec_recv) = self.driver.0.lock().receive() {
+            buf.copy_from_slice(&vec_recv);
+            Ok(vec_recv.len())
+        } else {
+            Err(DeviceError::NotReady)
+        }
+    }
+
+    fn send(&self, data: &[u8]) -> DeviceResult<usize> {
+        use smoltcp::phy::TxToken;
+        let token = E1000TxToken(self.driver.clone());
+        if token
+            .consume(Instant::from_millis(0), data.len(), |buffer| {
+                buffer.copy_from_slice(&data);
+                Ok(())
+            })
+            .is_ok()
+        {
+            Ok(data.len())
+        } else {
+            Err(DeviceError::NotReady)
+        }
+    }
+}
+
+pub struct E1000RxToken(Vec<u8>);
+pub struct E1000TxToken(E1000Driver);
+
+impl phy::Device<'_> for E1000Driver {
+    type RxToken = E1000RxToken;
+    type TxToken = E1000TxToken;
+
+    fn receive(&mut self) -> Option<(Self::RxToken, Self::TxToken)> {
+        self.0
+            .lock()
+            .receive()
+            .map(|vec| (E1000RxToken(vec), E1000TxToken(self.clone())))
+    }
+
+    fn transmit(&mut self) -> Option<Self::TxToken> {
+        if self.0.lock().can_send() {
+            Some(E1000TxToken(self.clone()))
+        } else {
+            None
+        }
+    }
+
+    fn capabilities(&self) -> DeviceCapabilities {
+        let mut caps = DeviceCapabilities::default();
+        caps.max_transmission_unit = 1536;
+        caps.max_burst_size = Some(64);
+        caps
+    }
+}
+
+impl phy::RxToken for E1000RxToken {
+    fn consume<R, F>(mut self, _timestamp: Instant, f: F) -> Result<R>
+    where
+        F: FnOnce(&mut [u8]) -> Result<R>,
+    {
+        f(&mut self.0)
+    }
+}
+
+impl phy::TxToken for E1000TxToken {
+    fn consume<R, F>(self, _timestamp: Instant, len: usize, f: F) -> Result<R>
+    where
+        F: FnOnce(&mut [u8]) -> Result<R>,
+    {
+        let mut buffer = [0u8; PAGE_SIZE];
+        let result = f(&mut buffer[..len]);
+
+        let mut driver = (self.0).0.lock();
+        driver.send(&buffer);
+
+        result
+    }
+}
+
+// JudgeDuck-OS/kern/e1000.c
+pub fn init(
+    name: String,
+    irq: usize,
+    header: usize,
+    size: usize,
+    index: usize,
+) -> DeviceResult<E1000Interface> {
+    info!("Probing e1000 {}", name);
+
+    // randomly generated
+    let mac: [u8; 6] = [0x54, 0x51, 0x9F, 0x71, 0xC0, index as u8];
+
+    let e1000 = E1000::new(header, size, DriverEthernetAddress::from_bytes(&mac));
+
+    let net_driver = E1000Driver(Arc::new(Mutex::new(e1000)));
+
+    let ethernet_addr = EthernetAddress::from_bytes(&mac);
+    //let ip_addrs = [IpCidr::new(IpAddress::v4(10, 0, index as u8, 2), 24)];
+    let ip_addrs = [IpCidr::new(IpAddress::v4(10, 0, 2, 15), 24)];
+    let neighbor_cache = NeighborCache::new(BTreeMap::new());
+    let iface = InterfaceBuilder::new(net_driver.clone())
+        .ethernet_addr(ethernet_addr)
+        .neighbor_cache(neighbor_cache)
+        .ip_addrs(ip_addrs)
+        .finalize();
+
+    info!("e1000 interface {} up with addr 10.0.{}.2/24", name, index);
+    let e1000_iface = E1000Interface {
+        iface: Mutex::new(iface),
+        driver: net_driver.clone(),
+        name,
+        irq,
+    };
+
+    Ok(e1000_iface)
+}
--- linux-object/src/process.rs
+++ linux-object/src/process.rs
@@ -17,7 +17,6 @@ use core::sync::atomic::AtomicI32;
 use hashbrown::HashMap;
 use kernel_hal::VirtAddr;
 use rcore_fs::vfs::{FileSystem, INode};
-use smoltcp::socket::SocketHandle;
 use spin::{Mutex, MutexGuard};
 
 use zircon_object::{
@@ -170,7 +169,7 @@ struct LinuxProcessInner {
     /// Signal actions
     signal_actions: SignalActions,
     /// Sockets
-    sockets: HashMap<SocketHandle, Arc<Mutex<dyn Socket>>>,
+    sockets: HashMap<usize, Arc<Mutex<dyn Socket>>>,
 }
 
 #[derive(Clone)]
@@ -332,9 +331,9 @@ impl LinuxProcess {
     }
 
     /// Add a socket to the socket set at given `SocketHandle`.
-    pub fn add_socket(&self, socket: Arc<Mutex<dyn Socket>>) -> LxResult<SocketHandle> {
+    pub fn add_socket(&self, socket: Arc<Mutex<dyn Socket>>) -> LxResult<usize> {
         let inner = self.inner.lock();
-        let fd = inner.get_free_hd();
+        let fd = inner.get_free_socket_fd();
         self.insert_socket(inner, fd, socket)
         // unimplemented!()
     }
@@ -343,9 +342,9 @@ impl LinuxProcess {
     fn insert_socket(
         &self,
         mut inner: MutexGuard<LinuxProcessInner>,
-        fd: SocketHandle,
+        fd: usize,
         socket: Arc<Mutex<dyn Socket>>,
-    ) -> LxResult<SocketHandle> {
+    ) -> LxResult<usize> {
         if inner.sockets.len() < inner.file_limit.cur as usize {
             inner.sockets.insert(fd, socket);
             Ok(fd)
@@ -355,7 +354,7 @@ impl LinuxProcess {
     }
 
     /// Get the `Socket` with given `fd`.
-    pub fn get_socket(&self, fd: SocketHandle) -> LxResult<Arc<Mutex<dyn Socket>>> {
+    pub fn get_socket(&self, fd: usize) -> LxResult<Arc<Mutex<dyn Socket>>> {
         // unimplemented!()
         let inner = self.inner.lock();
         let socket = inner.sockets.get(&fd).cloned().ok_or(LxError::EBADF);
@@ -363,7 +362,7 @@ impl LinuxProcess {
     }
 
     /// Close file descriptor `fd`.
-    pub fn close_socket(&self, fd: SocketHandle) -> LxResult {
+    pub fn close_socket(&self, fd: usize) -> LxResult {
         let mut inner = self.inner.lock();
         inner.sockets.remove(&fd).map(|_| ()).ok_or(LxError::EBADF)
     }
@@ -507,9 +506,8 @@ impl LinuxProcessInner {
             .unwrap()
     }
 
-    fn get_free_hd(&self) -> SocketHandle {
+    fn get_free_socket_fd(&self) -> usize {
         (SOCKET_FD..)
-            .map(|i| i.into())
             .find(|fd| !self.sockets.contains_key(fd))
             .unwrap()
     }
