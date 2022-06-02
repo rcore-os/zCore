fbe17c8454747a924ecccbf1d362bb40217193a5
elliott10 <xiaoluoyuan@163.com>
2022-06-02 11:24:12 +0000

    Deploying to gh-pages from  @ c7e10d3e1792c663748461f82ad19ed7a19a922c 🚀

--- xtask/arch/sidebar-items.js
+++ xtask/arch/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["fuse","制作镜像。"],["libos_libc_so","下载适用于 libos 的 musl libc so。"],["riscv64_linux_musl_cross","下载 riscv64-musl 工具链。"]],"struct":[["ArchArg",""]]});
+initSidebarItems({"fn":[["fuse","制作镜像。"],["libos_libc_so","下载适用于 libos 的 musl libc so。"],["linux_musl_cross","下载 musl 工具链。"]],"struct":[["ArchArg",""]]});
