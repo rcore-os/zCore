5cb95b0276108f304b9ea6deccd5c4f1a36df240
Luchangcheng2333 <1295470449@qq.com>
2022-06-11 10:34:44 +0800

    Update mod.rs

--- xtask/src/linux/mod.rs
+++ xtask/src/linux/mod.rs
@@ -104,7 +104,7 @@ impl LinuxRootfs {
                 let mut aarch64_tar = Tar::xf(&aarch64_tar, Some(&fw_dir));
                 aarch64_tar.invoke();
                 let boot_dir = "zCore/disk/EFI/Boot";
-                fs::create_dir_all(boot_dir)?;
+                fs::create_dir_all(boot_dir).ok();
                 fs::copy(fw_dir.join("aarch64_uefi.efi"), boot_dir);
                 fs::copy(fw_dir.join("Boot.json"), boot_dir);
             }
