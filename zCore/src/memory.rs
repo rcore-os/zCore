e9c47b92406865c49c890d5552e71e58f0e83e7d
YdrMaster <ydrml@hotmail.com>
2022-10-13 15:52:52 +0800

    docs: 补充一些文档
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- zCore/src/memory.rs
+++ zCore/src/memory.rs
@@ -1,4 +1,4 @@
-//! Define physical frame allocation and dynamic memory allocation.
+//! Define dynamic memory allocation.
 
 use crate::platform::phys_to_virt_offset;
 use alloc::alloc::handle_alloc_error;
@@ -12,11 +12,13 @@ use customizable_buddy::{BuddyAllocator, LinkedListBuddy, UsizeBuddy};
 use kernel_hal::PhysAddr;
 use lock::Mutex;
 
-/// 标准分配器类型。
-type MutAllocator<const N: usize> = BuddyAllocator<N, UsizeBuddy, LinkedListBuddy>;
-
 /// 堆分配器。
-static HEAP: Mutex<MutAllocator<27>> = Mutex::new(MutAllocator::new()); // 27 + 6 + 3  = 36 -> 16 GiB
+///
+/// 27 + 6 + 3 = 36 -> 64 GiB
+struct LockedHeap(Mutex<BuddyAllocator<27, UsizeBuddy, LinkedListBuddy>>);
+
+#[global_allocator]
+static HEAP: LockedHeap = LockedHeap(Mutex::new(BuddyAllocator::new()));
 
 /// 单页地址位数。
 const PAGE_BITS: usize = 12;
@@ -24,8 +26,53 @@ const PAGE_BITS: usize = 12;
 /// 为启动准备的初始内存。
 static mut MEMORY: [u8; 4 * 4096] = [0u8; 4 * 4096];
 
+unsafe impl GlobalAlloc for LockedHeap {
+    #[inline]
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        if let Ok((ptr, _)) = self.0.lock().allocate_layout(layout) {
+            ptr.as_ptr()
+        } else {
+            handle_alloc_error(layout)
+        }
+    }
+
+    #[inline]
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        self.0
+            .lock()
+            .deallocate_layout(NonNull::new(ptr).unwrap(), layout)
+    }
+}
+
+/// 初始化分配器，并将一个小的内存块注册到分配器中，用于启动需要的动态内存。
+pub fn init() {
+    unsafe {
+        log::info!("MEMORY = {:#?}", MEMORY.as_ptr_range());
+        let mut heap = HEAP.0.lock();
+        let ptr = NonNull::new(MEMORY.as_mut_ptr()).unwrap();
+        heap.init(core::mem::size_of::<usize>().trailing_zeros() as _, ptr);
+        heap.transfer(ptr, MEMORY.len());
+    }
+}
+
+/// 将一些内存区域注册到分配器。
+pub fn insert_regions(regions: &[Range<PhysAddr>]) {
+    let mut heap = HEAP.0.lock();
+    let offset = phys_to_virt_offset();
+    regions
+        .iter()
+        .filter(|region| !region.is_empty())
+        .for_each(|region| unsafe {
+            heap.transfer(
+                NonNull::new_unchecked((region.start + offset) as *mut u8),
+                region.len(),
+            );
+        });
+}
+
 pub fn frame_alloc(frame_count: usize, align_log2: usize) -> Option<PhysAddr> {
     let (ptr, size) = HEAP
+        .0
         .lock()
         .allocate::<u8>(align_log2 << PAGE_BITS, unsafe {
             NonZeroUsize::new_unchecked(frame_count << PAGE_BITS)
@@ -36,52 +83,8 @@ pub fn frame_alloc(frame_count: usize, align_log2: usize) -> Option<PhysAddr> {
 }
 
 pub fn frame_dealloc(target: PhysAddr) {
-    HEAP.lock().deallocate(
+    HEAP.0.lock().deallocate(
         unsafe { NonNull::new_unchecked((target + phys_to_virt_offset()) as *mut u8) },
         1 << PAGE_BITS,
     );
-}
-
-pub fn init() {
-    unsafe {
-        log::info!("MEMORY = {:#?}", MEMORY.as_ptr_range());
-        let mut heap = HEAP.lock();
-        let ptr = NonNull::new(MEMORY.as_mut_ptr()).unwrap();
-        heap.init(core::mem::size_of::<usize>().trailing_zeros() as _, ptr);
-        heap.transfer(ptr, MEMORY.len());
-    }
-}
-
-pub fn insert_regions(regions: &[Range<PhysAddr>]) {
-    let offset = phys_to_virt_offset();
-    regions
-        .iter()
-        .filter(|region| !region.is_empty())
-        .for_each(|region| unsafe {
-            HEAP.lock().transfer(
-                NonNull::new_unchecked((region.start + offset) as *mut u8),
-                region.len(),
-            );
-        });
-}
-
-struct Global;
-
-#[global_allocator]
-static GLOBAL: Global = Global;
-
-unsafe impl GlobalAlloc for Global {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        if let Ok((ptr, _)) = HEAP.lock().allocate_layout::<u8>(layout) {
-            ptr.as_ptr()
-        } else {
-            handle_alloc_error(layout)
-        }
-    }
-
-    #[inline]
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        HEAP.lock()
-            .deallocate_layout(NonNull::new(ptr).unwrap(), layout)
-    }
 }
