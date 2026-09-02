05d076b74d620e8881f060f60378c8fa11c53917
Runji Wang <runji@deepseek.com>
2026-09-03 00:28:05 +0800

    ci: use packaged QEMU

--- .github/scripts/make-qemu.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://download.qemu.org/qemu-$1.tar.xz
-tar -xJf qemu-$1.tar.xz
-cd qemu-$1
-./configure --target-list=x86_64-softmmu,riscv64-softmmu,aarch64-softmmu
-make -j > /dev/null 2>&1
