c373b6e1a398acf9cd82b5850110a817192d53ea
Runji Wang <runji@deepseek.com>
2026-09-03 00:13:12 +0800

    build: refresh macro dependencies

--- xtask/src/build.rs
+++ xtask/src/build.rs
@@ -111,6 +111,7 @@ impl BuildConfig {
             .package("zcore")
             .features(false, &self.features)
             .target(INNER.join(format!("{}.json", self.arch.name())))
+            .args(["-Z", "json-target-spec"])
             .args(["-Z", "build-std=core,alloc"])
             .args(["-Z", "build-std-features=compiler-builtins-mem"])
             .conditional(!self.debug, |cargo| {
