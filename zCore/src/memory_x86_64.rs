11a592b335855aad14aab46e16c6cb2132bb3571
Runji Wang <runji@deepseek.com>
2026-09-03 18:11:38 +0800

    fix(x86_64): boot current Zircon tests

--- zCore/src/memory_x86_64.rs
+++ zCore/src/memory_x86_64.rs
@@ -2,6 +2,8 @@
 
 use bitmap_allocator::BitAlloc;
 use core::ops::Range;
+#[cfg(not(feature = "libos"))]
+use kernel_hal::mem::phys_to_virt;
 use kernel_hal::sync::Mutex;
 use kernel_hal::PhysAddr;
 
@@ -25,11 +27,44 @@ fn frame_idx_to_phys_addr(idx: usize) -> PhysAddr {
 pub fn insert_regions(regions: &[Range<PhysAddr>]) {
     debug!("init_frame_allocator regions: {regions:x?}");
     let mut ba = FRAME_ALLOCATOR.lock();
+    #[cfg(not(feature = "libos"))]
+    const DYNAMIC_HEAP_SIZE: usize = 256 * 1024 * 1024;
+    #[cfg(not(feature = "libos"))]
+    let heap_region = regions.iter().find_map(|region| {
+        let start = (region.start + DYNAMIC_HEAP_SIZE - 1) & !(DYNAMIC_HEAP_SIZE - 1);
+        (start + DYNAMIC_HEAP_SIZE <= region.end).then_some(start..start + DYNAMIC_HEAP_SIZE)
+    });
+    #[cfg(not(feature = "libos"))]
+    if let Some(region) = &heap_region {
+        unsafe {
+            HEAP_ALLOCATOR
+                .lock()
+                .add_to_heap(phys_to_virt(region.start), phys_to_virt(region.end));
+        }
+        info!("Dynamic heap region: {region:#x?}");
+    }
+
     for region in regions {
         let frame_start = phys_addr_to_frame_idx(region.start);
         let frame_end = phys_addr_to_frame_idx(region.end - 1) + 1;
-        if frame_start < frame_end {
+
+        #[cfg(not(feature = "libos"))]
+        if let Some(heap) = heap_region
+            .as_ref()
+            .filter(|heap| region.start <= heap.start && heap.end <= region.end)
+        {
+            let heap_start = phys_addr_to_frame_idx(heap.start);
+            let heap_end = phys_addr_to_frame_idx(heap.end);
+            if frame_start < heap_start {
+                ba.insert(frame_start..heap_start);
+            }
+            if heap_end < frame_end {
+                ba.insert(heap_end..frame_end);
+            }
+        } else if frame_start < frame_end {
             ba.insert(frame_start..frame_end);
+        }
+        if frame_start < frame_end {
             info!(
                 "Frame allocator: add range {:#x?}",
                 frame_idx_to_phys_addr(frame_start)..frame_idx_to_phys_addr(frame_end),
