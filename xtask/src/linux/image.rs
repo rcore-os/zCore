4b027e7c45d8ac6298fd8a71e651552fe6ca339c
Theo Paris <theo@theoparis.com>
2026-09-03 22:47:21 -0700

    Use rboot instead of rayboot
    Signed-off-by: Theo Paris <theo@theoparis.com>

--- xtask/src/linux/image.rs
+++ xtask/src/linux/image.rs
@@ -1,5 +1,5 @@
-use crate::{commands::wget, Arch, PROJECT_DIR};
-use os_xtask_utils::{dir, CommandExt, Qemu, Tar};
+use crate::{Arch, PROJECT_DIR};
+use os_xtask_utils::{dir, CommandExt, Ext, Qemu};
 use std::{fs, path::Path};
 
 impl super::LinuxRootfs {
@@ -10,24 +10,31 @@ impl super::LinuxRootfs {
         // 镜像路径
         let inner = PROJECT_DIR.join("zCore");
         let image = inner.join(format!("{arch}.img", arch = self.0.name()));
-        // aarch64 还需要下载 firmware
+        // aarch64 升级为 rboot
         if let Arch::Aarch64 = self.0 {
-            const URL: &str = "https://github.com/Luchangcheng2333/rayboot/releases/download/2.0.0/aarch64_firmware.tar.gz";
-            let aarch64_tar = self.0.origin().join("Aarch64_firmware.zip");
-            wget(URL, &aarch64_tar);
-
-            let fw_dir = self.0.target().join("firmware");
-            dir::clear(&fw_dir).unwrap();
-            Tar::xf(&aarch64_tar, Some(&fw_dir)).invoke();
-
             let boot_dir = inner.join("disk").join("EFI").join("Boot");
             dir::clear(&boot_dir).unwrap();
-            fs::copy(
-                fw_dir.join("aarch64_uefi.efi"),
-                boot_dir.join("bootaa64.efi"),
-            )
-            .unwrap();
-            fs::copy(fw_dir.join("Boot.json"), boot_dir.join("Boot.json")).unwrap();
+
+            Ext::new("cargo")
+                .arg("build")
+                .arg("--manifest-path")
+                .arg("rboot/Cargo.toml")
+                .arg("--target")
+                .arg("aarch64-unknown-uefi")
+                .arg("-Zbuild-std=core,alloc")
+                .arg("-Zbuild-std-features=compiler-builtins-mem")
+                .arg("--release")
+                .invoke();
+
+            let rboot_efi = PROJECT_DIR
+                .join("rboot")
+                .join("target")
+                .join("aarch64-unknown-uefi")
+                .join("release")
+                .join("rboot.efi");
+
+            fs::copy(&rboot_efi, boot_dir.join("bootaa64.efi")).unwrap();
+            fs::copy(inner.join("rboot.conf"), boot_dir.join("rboot.conf")).unwrap();
         }
         // 生成镜像
         fuse(self.path(), &image);
